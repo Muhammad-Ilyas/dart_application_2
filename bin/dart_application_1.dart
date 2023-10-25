000cc3ca30c80b5ebe2e754a337a2e6dedc90ea4
Muhammad Ilyas <muhammad_ilyas192@hotmail.com>
2023-10-25 19:48:48 +0500

    adding-company-name

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -5,4 +5,8 @@ void main() {
   stdout.write('What is your name: ');
   var name = stdin.readLineSync();
   stdout.write("My name is $name");
+
+  stdout.write('What is your name: ');
+  var company = stdin.readLineSync();
+  stdout.write("My company name is $company");
 }
